85bfa4d1ce00cfbdfaf8ccd58e0a1fe6c9fc0ee1
ckopecky <kopecky12112@gmail.com>
2020-04-07 19:48:59 -0700

    add style

--- countries/src/components/CountryDetail/CountryStyle.js
+++ countries/src/components/CountryDetail/CountryStyle.js
@@ -1,8 +1,42 @@
+//detail
 import styled from 'styled-components';
 
-export const CountryStyle = styled.section`
-    background: ${props => props.theme.elements};
-    padding: 20px;
-    border-radius: 10px;
+export const CountryStyle = styled.div`
+    display: flex;
+    width: 100%;
+    margin: 20px auto;
+    div:first-of-type {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        h3 {
+            width: 50%;
+            font-weight: bolder;
+            font-size: 3rem;
+            line-height: 2;
+            margin: 20px;
+        }
+        img {
+            width: 50%;
+        }
+        .styled-div{
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            margin: 20px;
+        }
 
+        .section {
+            width: 50%;
+            p {
+                line-height: 2.5;
+                font-size: 1.6rem;
+
+            }
+            p > .headings {
+                font-weight: bolder;
+            }
+        }
+    }
+    
 `;
