507a22f8cb41c13cd2e73c07d50f9fdd8a5460c0
ckopecky <kopecky12112@gmail.com>
2020-04-09 10:05:23 -0700

    format population correctly

--- countries/src/components/CountryMain/CountryMain.js
+++ countries/src/components/CountryMain/CountryMain.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { CountryStyle, CountryStyledLink } from './CountryStyle';
 
 const CountryMain = (props) => {
-    const { name, flag, population, region, capital } = props.country;
-    const formatPopulation = Number(population).toLocaleString();
+    const { name, flag, region, capital } = props.country;
+    const population = Number(props.country.population).toLocaleString();
+    props.country.population = population;
     return (
         <CountryStyle id={props.alpha3Code}>
-            <CountryStyledLink name="country" to={{pathname: `/${name}`, state: {country: props.country}}}>
+            <CountryStyledLink name="country" to={{pathname: `/${name}`, state: {country: props.country, formatPop: population } }}>
                 <img src={flag} height="180"  width="300" alt={name}/>
                 <h3 className="country-name">{name}</h3>
-                <p>Population: {formatPopulation}</p>
+                <p>Population: {population}</p>
                 <p>Region: {region}</p>
                 <p>Capital: {capital}</p>
             </CountryStyledLink>
